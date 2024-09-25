10b921ea972319d93766b46f4f3702c1aeb900b9
Ksawery Margas <ksawerymargas@gmail.com>
2024-09-25 09:14:19 +0200

    Add files via upload

---
+++ TicTacToe/TicTacToe/Player.cs
@@ -0,0 +1,23 @@
+﻿namespace TicTacToe
+{
+    internal class Player
+    {
+        private char activePlayer;
+        public Player()
+        {
+            activePlayer = 'O';
+        }
+        public string GetPlayer()
+        {
+            return activePlayer.ToString();
+        }
+        public void SwitchPlayer()
+        {
+            activePlayer = (activePlayer == 'O') ? 'X' : 'O';
+        }
+        public void Reset()
+        {
+            activePlayer = 'O';
+        }
+    }
+}
